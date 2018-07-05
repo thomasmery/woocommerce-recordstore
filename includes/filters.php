53fad550abf08fe42feae63fae735e111533b3b2
Thomas Mery <thomas.mery@gmail.com>
2018-07-05 18:31:23 +0200

    Enhance WooCommerce Related Products output
    * this commit
    
    - excludes the root Music Release Category from the category used to select related products to avoid too broad matches
    - adds similar Styles & Artists to the selection process

--- includes/filters.php
+++ includes/filters.php
@@ -35,3 +35,58 @@ add_filter(
         return $terms;
     }
 );
+
+
+/**
+ * Customize related products output
+ */
+
+add_filter(
+    'woocommerce_get_related_product_cat_terms',
+    function($terms_ids, $product_id) {
+        // exclude Music Release Root Category
+        // as by default all Music Releases belong to this parent category
+        // and that not excluding it would make the related products mechanism useless
+        // as all music releases could potentially be returned
+        $terms_ids = array_filter(
+            $terms_ids,
+            function($term_id) {
+                if (is_numeric($term_id)) {
+                    $term = get_term_by('term_id', $term_id, 'product_cat');
+                }
+
+                if(is_a($term, 'WP_Term')) {
+                    return $term->slug !== 'music-release';
+                }
+
+                return false;
+            }
+        );
+        return $terms_ids;
+    },
+    10,
+    2
+ );
+
+
+ /**
+  * Use the woocommerce_get_related_product_tag_terms filter
+  * to add WooCommerce Record Store custom taxonomies terms to be used
+  * in the related products fetch mechanism
+  * we could also use the filter used for product categories as the related products query
+  * does not seem to care for the taxonomy name ...
+  */
+ add_filter(
+    'woocommerce_get_related_product_tag_terms',
+    function($terms, $product_id) {
+        // get the music release genres ids
+        if(wc_recordstore_is_music_release($product_id)) {
+            $styles_ids = wp_get_object_terms( $product_id, STYLE_TAXONOMY, [ 'fields' => 'ids' ] );
+            $artist_ids = wp_get_object_terms( $product_id, ARTIST_TAXONOMY, [ 'fields' => 'ids' ] );
+            return array_merge($terms, $styles_ids, $artist_ids);
+        }
+        return $terms;
+    },
+    10,
+    2
+ );
